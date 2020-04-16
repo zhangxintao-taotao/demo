04bdddc7117c2625e8259f3a914361572b79c69e
jyk <jyk2510428714@163.com>
2020-04-16 17:31:15 +0800

    小组成员姜永康修改

--- AdminDaolmpl.java
+++ AdminDaolmpl.java
@@ -1,9 +1,10 @@
 public class AdminDaolmpl{
 
 public int login(){}
-public int register(){}
+public int register(){这里我给你加点料}
 public int add (){}
 public list<> findall(){}
 public int update(){}
 public intdelete(){}
+小组成员姜永康修改
 }
